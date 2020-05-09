cdd19658aa0ea10a91c465fb5885f73eace0020d
HeYin <374230654@qq.com>
2020-05-09 11:10:24 +0800

    测试zookeeper分布式锁

--- Lab1/src/main/java/com/work/learn/config/ZookeeperProperties.java
+++ Lab1/src/main/java/com/work/learn/config/ZookeeperProperties.java
@@ -2,7 +2,7 @@ package com.work.learn.config;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * @description: 注入 zookeeper 的配置信息
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
  * @version: 1.0
  */
 @Data
-@Component
+@Configuration
 @ConfigurationProperties(prefix = "zookeeper")
 public class ZookeeperProperties {
 
